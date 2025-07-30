8482e6be6c025f2913958cd148b3a9fafa674ff1
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2025-07-30 07:04:08 -0400

    Add test for blank raw API response; Stop backfilling every day (lol)

--- blockstream_dbt/tests/test_raw_response_not_blank.sql
+++ blockstream_dbt/tests/test_raw_response_not_blank.sql
@@ -1,8 +1,8 @@
 SELECT
   id,
-  raw_response[i] as missing_field,
-  i as index_in_array
-from {{ source('raw', 'raw_binance_us_24hr_ticker') }},
-     range(0, length(raw_response)) as t(i)
-WHERE raw_response[i] IS NULL OR trim(raw_response[i]) = ''
+  raw_response
+FROM {{ source('raw', 'raw_binance_us_24hr_ticker') }}
+WHERE raw_response IS NULL
+   OR TRIM(raw_response) = '[]'
+   OR TRIM(raw_response) = ''
 limit 10
